28ec673bcbb0076c8da9ceb66903f17ef86aca66
Printassia Bey <printassiajohnson@gmail.com>
2020-10-15 08:02:04 -0400

    Update Exercise3_3.java

--- Exercise3_3.java
+++ Exercise3_3.java
@@ -7,7 +7,7 @@ One way to estimate the adult height of a child is to use the following formula,
 which uses the height of the parents:
 Hmale_child = ((Hmother * 13/12) + Hfather) / 2
 Hfemale_child = ((Hfather * 12/13) + Hmother) / 2
- All heights are in inches. Write a program that takes as input the gender of the
+All heights are in inches. Write a program that takes as input the gender of the
 child, the height of the mother in inches, and the height of the father in inches,
 and outputs the estimated adult height of the child in inches. The program should
 allow the user to enter a new set of values and output the predicted height 
